701f13c018628e5ee512e32deaa849be10f5429c
Caleb Smith <46058761+ClamSageCaleb@users.noreply.github.com>
2019-08-14 22:07:47 -0400

    Added !km command

--- ropcs.py
+++ ropcs.py
@@ -15,15 +15,25 @@ import discord
 from discord import Game
 from discord.ext.commands import Bot
 
+# Bot prefix and Discord Bot token
 BOT_PREFIX = ("!")
 TOKEN = "NTg4NDM0NjAzNjM1OTAwNDI2.XUunhg.xyN0xKhOnRlnF0jba6p-knsL50o"
 
+# Creates the Bot with name 'client'
 client = Bot(command_prefix=BOT_PREFIX)
 client.remove_command('help')
 
+# Lists for information such as LoL Champs/Roles/Keystones/Items
 txt = 'champions.txt'
+itemsTxt = 'items.txt'
 champs = []
 roles = []
+items = []
+ks = ['Press the Attack', 'Lethal Tempo', 'Fleet Footwork', 'Conqueror', 'Electrocute', 'Predator', 'Dark Harvest',
+      'Hail of Blades', 'Summon Aery', 'Arcane Comet', 'Phase Rush', 'Grasp of the Undying', 'Aftershock Guardian',
+      'Glacial Augment', 'Kleptomancy', 'Unsealed Spellbook']
+
+# Reading the files to populate lists champs and items
 with open(txt) as f:
     for line in f:
         s = line.strip().split(' ')
@@ -31,9 +41,18 @@ with open(txt) as f:
         roles.append(s[1])
 champSelect = dict(zip(champs, roles))
 
+with open(itemsTxt) as f:
+    for line in f:
+        items.append(line)
 
+
+# Replaces the basic !help feature, responds with formatted bot commands and usage
 @client.event
 async def on_message(message):
+    """
+    :param message: Sent in by the user. Executed by !help
+    :return: Formatted command list
+    """
     if message.author == client.user:
         return
 
@@ -47,6 +66,8 @@ async def on_message(message):
         msg.add_field(name="Random Role", value="Selects a random LoL Role. \n Usage: !rr", inline=False)
         msg.add_field(name="Combo Role/Champ", value="Selects a random LoL Role and Champion. \n Usage: !rac",
                       inline=False)
+        msg.add_field(name="Kill me", value="Selects a random LoL Champ, Role, Keystone, and two items. \n Usage: !km",
+                      inline=False)
         msg.add_field(name="GG EZ", value="Just use it \n Usage: !ggez @[user]", inline=False)
         msg.set_thumbnail(url="https://themerkle.com/wp-content/uploads/2017/05/kingdice-pr.jpg")
         msg.set_footer(text="Developed by Clam")
@@ -56,6 +77,10 @@ async def on_message(message):
 
 @client.command(name='8ball', aliases=['eight_ball', 'eightball', '8-ball'], pass_context=True)
 async def eight_ball(context):
+    """
+    :param context: The question the user is wanting to ask
+    :return: Answer to the question
+    """
     possible_responses = [
         'That is a resounding no',
         'It is not looking likely',
@@ -68,6 +93,10 @@ async def eight_ball(context):
 
 @client.command()
 async def square(number):
+    """
+    :param number: The number the user is wanting to square
+    :return: Squared Number
+    """
     squared_value = int(number) * int(number)
     await client.say(str(number) + " squared is " + str(squared_value))
 
@@ -80,6 +109,9 @@ async def on_ready():
 
 @client.command()
 async def bitcoin():
+    """
+    :return: Price of bitcoin
+    """
     url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
     async with aiohttp.ClientSession() as session:  # Async HTTP request
         raw_response = await session.get(url)
@@ -88,31 +120,48 @@ async def bitcoin():
         await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
 
 
+# randomchamp command takes input !rc and replies with a random champion
 @client.command(aliases=['rc', 'rchamp'], pass_context=True)
 async def randomchamp(ctx):
+    """
+    :param ctx: Takes the user's message in order to extract the author.
+    :return: The user's champion selection
+    """
     await client.say("{0.mention}, Your Champ is: ".format(ctx.message.author) + random.choice(champs))
 
 
+# randomrole command takes input !rr and replies with a random role
 @client.command(aliases=['rr', 'rrole'], pass_context=True)
 async def randomrole(ctx):
+    """
+    :param ctx: Takes the user's message in order to extract the author.
+    :return: The user's role selection
+    """
     await client.say("{0.mention}, Your Role is: ".format(ctx.message.author) + random.choice(roles))
 
 
+# bothroleandchamp command takes input !rac and replies with a role and champion
 @client.command(aliases=['rac', 'rolechamp'], pass_context=True)
 async def bothroleandchamp(ctx):
+    """
+    :param ctx: Takes the user's message in order to extract the author.
+    :return: The user's role and champion selection
+    """
     await client.say("{0.mention}, \n Your Champ is: ".format(ctx.message.author) + random.choice(
         champs) + "\n Your Role is: " + random.choice(roles))
 
 
-#TODO: Add functioanlity for this command
-#@client.command(aliases=['km'], pass_context=True)
-#async def killme(ctx):
-    #Output:
-    #>!km
-    #Champ: [champ]
-    #Role: [role]
-    #Keystone: [keystone]
-    #Items: [item1] [item2]
+@client.command(aliases=['km'], pass_context=True)
+async def killme(ctx):
+    """
+    :param ctx: Takes the user's message in order to extract the author
+    :return: Returns the users Champ, Role, Keystone, and two Items
+    """
+    # TODO: Potentially add 6 random items, also add an if statement where if the role is jungle at least one item
+    #  will be a jungle item.
+    await client.say("{0.mention}, \n Your Champ is: ".format(ctx.message.author) + random.choice(
+        champs) + "\n Your Role is: " + random.choice(roles) + "\n Your keystone is: " + random.choice(
+        ks) + "\n Your two items are: " + random.choice(items) + ", " + random.choice(items))
 
 
 @client.command()
