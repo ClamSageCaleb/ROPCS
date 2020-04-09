4321207b171c14bc079504d59126431b2a0a0ea9
Caleb Smith <caleb.benjamin9799@gmail.com>
2020-04-09 16:53:39 -0400

    Potential fix for delete message

--- ropcs.py
+++ ropcs.py
@@ -323,7 +323,7 @@ async def summoner(ctx, name):
 @client.command(aliases=['v'], pass_context=True)
 async def vote(ctx, *args):
     question = '{}'.format(' '.join(args))
-    await client.delete_message(ctx.message)
+    await ctx.message.delete()
     msg = discord.Embed(title='__**Place your Votes!**__', description="", color=0x0000ff)
     msg.add_field(name=question, value= "<✅ for yes! \n ❌ for no!", inline=False)
 
